b9f050c4290a90571ccb4e0a41dbf981acfec6fa
Ildus K <k-dus@yandex.ru>
2014-03-27 13:24:34 +0800

    ip used for client instead of 'localhost', pep8 changes

--- aggregate/client.py
+++ aggregate/client.py
@@ -1,6 +1,6 @@
 import threading
+import zmq
 
-import zmq 
 
 class _RemoteMethod:
     def __init__(self, socket, name):
@@ -11,29 +11,30 @@ class _RemoteMethod:
         self.socket.send_pyobj((self.name, args, kwargs))
         return self.socket.recv_pyobj()
 
+
 class Aggregator(object):
     def __init__(self):
         self.context = zmq.Context()
         self.data_socket = self.context.socket(zmq.PUB)
-        self.data_socket.connect ("tcp://localhost:5556")
+        self.data_socket.connect("tcp://127.0.0.1:5556")
         self.control_socket = self.context.socket(zmq.REQ)
-        self.control_socket.connect("tcp://localhost:5557")
-    
+        self.control_socket.connect("tcp://127.0.0.1:5557")
+
     def insert(self, tags, values):
         self.insert_all([(tags, values)])
-        
+
     def insert_all(self, items):
         self.data_socket.send_pyobj(items)
-        
-        
+
     def __getattr__(self, name):
         return _RemoteMethod(self.control_socket, name)
-    
+
     def ping(self):
         self.data_socket.send_pyobj(None)
 
 
 _local = threading.local()
+
 
 def get_client():
     try:
